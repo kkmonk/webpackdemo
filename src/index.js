28b4f73ce0ff5525cf505e01892a7d9b40759e1a
kkmonk <745147294@qq.com>
2018-03-14 14:10:02 +0800

    add css and img

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-import _ from 'lodash'
+import _ from 'lodash';
+import './style.css';
+import Icon from './icon.png'
+
+import printMe from './print.js'
+
 function component() {
   var element = document.createElement('div');
-
+  var btn = document.createElement('button');
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  //btn
+  btn = innerHTML = 'click me and check the console';
+  btn.onclick = printMe;
+  //add btn
+  element.appendChild(btn);
+
+  //add the image
+  var myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
 
   return element;
 }
